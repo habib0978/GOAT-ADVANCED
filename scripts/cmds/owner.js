fa4baa879383a620da0e3cb7885d96cfa56bed84
habib0978 <arminakhatun7865@gmail.com>
2026-02-28 00:07:19 +0530

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -18,17 +18,17 @@ module.exports = {
   onStart: async function ({ api, event }) {
     const ownerText = 
 `╭─ 👑 Oᴡɴᴇʀ Iɴғᴏ 👑 ─╮
-│ 👤 Nᴀᴍᴇ       : Mᴏʜᴀᴍᴍᴀᴅ Aᴋᴀsʜ
-│ 🧸 Nɪᴄᴋ       : Aᴋᴀsʜ
+│ 👤 Nᴀᴍᴇ       : 𝚂𝙺 𝙷𝙰𝙱𝙸𝙱
+│ 🧸 Nɪᴄᴋ       : 𝙷𝚊𝚋𝚒𝚋
 │ 🎂 Aɢᴇ        : 18+
 │ 💘 Rᴇʟᴀᴛɪᴏɴ : Sɪɴɢʟᴇ
-│ 🎓 Pʀᴏғᴇssɪᴏɴ : Sᴛᴜᴅᴇɴᴛ
-│ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : Iɴᴛᴇʀ 2ɴᴅ Yᴇᴀʀ
-│ 🏡 Lᴏᴄᴀᴛɪᴏɴ : 𝐃𝐡𝐚𝐤𝐚 - 𝐆𝐚𝐳𝐢𝐩𝐮𝐫
+│ 🎓 Pʀᴏғᴇssɪᴏɴ : 𝚆𝚘𝚛𝚔𝚎𝚛
+│ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : 𝚂𝚘𝚋 𝚙𝚊𝚜𝚜
+│ 🏡 Lᴏᴄᴀᴛɪᴏɴ : 𝐊𝐨𝐥𝐤𝐚𝐭𝐚-𝐇𝐨𝐨𝐠𝐡𝐥𝐲
 ├─ 🔗 Cᴏɴᴛᴀᴄᴛ ─╮
-│ 📘 Facebook  : fb.com/akashx404 
-│ 💬 Messenger: m.me/akashx404 
-│ 📞 WhatsApp  : wa.me/01933165880
+│ 📘 Facebook  : www.facebook.com/share/1B58uQgP11/ 
+│ 💬 Messenger: /m.me/Bhodro.habib 
+│ 📞 WhatsApp  : wa.me/+919564045875
 ╰────────────────╯`;
 
     const cacheDir = path.join(__dirname, "cache");
@@ -36,7 +36,7 @@ module.exports = {
 
     if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir);
 
-    const imgLink = "https://i.imgur.com/1G4ZhU7.jpeg";
+    const imgLink = "https://i.imgur.com/IphuyKF.jpeg";
 
     const send = () => {
       api.sendMessage(
